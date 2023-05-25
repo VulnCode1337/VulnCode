2374e1e7df477ff9aba371e6126e03814b798c9b
VulnCode1337 <Vulncode1337@gmail.com>
2023-05-25 16:13:30 -0400

    updated db connection function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const passport = require('passport');
 const multer = require('multer');
 const flash = require('connect-flash');
+const MongoStore = require('connect-mongo');
 
 require('./config/passport')(passport); // Passport configuration
 
@@ -19,10 +20,14 @@ app.use(express.static('public'));
 
 
 app.use(session({
-  secret: '!@#$1234',
-  resave: false,
-  saveUninitialized: false,
-}));
+    secret: '!@#$1234',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ 
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/vulncodeserverDB',
+        ttl: 14 * 24 * 60 * 60 // = 14 days. Default
+    })
+  }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -48,8 +53,9 @@ app.set('views', path.join(__dirname, 'views'));
 db.connectToMongo()
     .then(() => {
         // start the server
-        app.listen(3000, () => {
-            console.log('Server started on port 3000');
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log('Server started on port ${port}');
         });
     })
     .catch((err) => {
